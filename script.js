b71ef45c237e29faf5c14021dd4ab6ed9515d2f1
hariikr <90984013+hariikr@users.noreply.github.com>
2022-03-10 21:19:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,22 +9,3 @@
     typeSpeed:145,
     showCursor:false,
     })
-
-
-    $("#submit-form").submit((e)=>{
-        e.preventDefault()
-        $.ajax({
-            url:"https://script.google.com/macros/s/AKfycbzBBfve8ZvRXOqsnN7_0JDGnaV8IB3Ld6E7Qurha30XczXbgYkt1vAKpPLVrpK_u90/exec",
-            data:$("#submit-form").serialize(),
-            method:"get",
-            success:function (response){
-                alert("Form submitted successfully")
-                window.location.reload()
-                //window.location.href="https://google.com"
-            },
-            error:function (err){
-                alert("Something Error")
-
-            }
-        })
-    })
